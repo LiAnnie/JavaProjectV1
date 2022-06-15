7eb56cb625b0aded9937a6d5e8725bfaee2caad4
ShaikhMehreen <86336962+ShaikhMehreen@users.noreply.github.com>
2022-06-15 18:40:41 -0400

    Update Instructions.java

--- Instructions.java
+++ Instructions.java
@@ -37,9 +37,17 @@ public class Instructions {
 		frame.getContentPane().setBackground(Color.ORANGE);
 		
 		// declaring the JLabels
-		JLabel label = new JLabel("<html><body>Instructions: <br>Step 1<br>Step 2<br>Step 3</body></html>");
-		label.setFont(new Font("Diolog", Font.BOLD, 28));
-		label.setBounds(100, 100, 300, 300);
+		JLabel label = new JLabel("<html><body>This game is a mystery game where you need to find out who\r\n"
+				+ "stole the Mona Lisa! There are four suspects, each who have their"
+				+ " own story to tell. Who will turn out to be the culprit?"
+				+ " When clicking start, you will be redirected to a map.\r\n"
+				+ "<br>In that map there are several buttons that lead to different\r\n"
+				+ "<br>rooms. In each room there is a suspect who you must interrogate\r\n"
+				+ "<br>and search! To learn more about your suspects, make sure to play\r\n"
+				+ "<br>the mini games while searching to find more clues. Good Luck \r\n"
+				+ "<br>Detective, the museum is counting on you! <br></body></html>");
+		label.setFont(new Font("Diolog", Font.BOLD, 20));
+		label.setBounds(150, 60, 600, 300);
 		frame.add(label);
 		
 		// BACK BUTTON
